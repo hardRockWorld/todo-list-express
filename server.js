3e7c70d1f8dbb2acf4bf700b94ff7316dc15ec0b
Gourab Mahapatra <mgourab@hotmail.com>
2023-05-29 02:27:01 +0530

    - change DB_STRING to cyclic name

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ require('dotenv').config()
 
 
 let db,
-    dbConnectionStr = process.env.DB_STRING,
+    dbConnectionStr = process.env.MONGO_CONNECTION_STRING,
     dbName = 'todo'
 
 MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
